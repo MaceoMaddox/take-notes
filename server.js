81307b4e492e0be2b213c04d00a26a71f1abae32
SonSonder <maceomaddox@gmail.com>
2022-09-29 15:59:39 -0400

    ......app won't.. do anything...

--- server.js
+++ server.js
@@ -1,5 +1,16 @@
-// const express = require("express");
-// const fs = require("fs");
-// const path = require("path");
-// const app = express();
-// const PORT = process.env.PORT||5500
+const express = require("express");
+const fs = require("fs");
+const path = require("path");
+const app = express();
+const PORT = process.env.PORT||5500
+const db = require("./db/db.json")
+
+app.use(express.urlencoded({extended: true}));
+app.use(express.json());
+app.use(express.static(__dirname));
+
+require("./routes/routes")(app);
+
+app.listen(PORT, function() {
+    console.log(`taking notes in http://localhost:${PORT}`)
+})
